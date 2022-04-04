6d67704bc85d158498b8e9f01ac416fcaaf4768a
Nayim hossen <nayimh2015@gmail.com>
2022-04-04 17:38:44 +0600

    context

--- src/Components/Aunty/Aunty.js
+++ src/Components/Aunty/Aunty.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../GrandPa/GrandPa';
 
 const Aunty = ({ house }) => {
+
+    const ring = useContext(RingContext);
     return (
         <div>
             <h2>This is aunty</h2>
             <h3>House: {house}</h3>
+            <h4>Ornament: {ring}</h4>
         </div>
     );
 };
